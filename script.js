a7052b39fc1dfa4c4cf73a6f61c3f60a14da503c
9064abc <163479544+9064abc@users.noreply.github.com>
2024-05-21 16:09:51 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var context　= cvs.getContext("2d");
 context.fillStyle = "blue";
 context.fillRect(0,0,440,440);
 var base = new Image();
-var groung = new Image();
+var ground = new Image();
 var flag = new Image();
 //flag.src = "logo_url";
 base.src = "base.png";
