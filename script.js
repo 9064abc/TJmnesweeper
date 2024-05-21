86a6601e90db524409ef64b3a56402a68bcfad58
9064abc <163479544+9064abc@users.noreply.github.com>
2024-05-21 16:07:38 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,27 @@ var cvs = document.getElementById("minesweeper");
 var context　= cvs.getContext("2d");
 context.fillStyle = "blue";
 context.fillRect(0,0,440,440);
+var base = new Image();
+var groung = new Image();
+var flag = new Image();
+//flag.src = "logo_url";
+base.src = "base.png";
+ground.src = "ground.png";
 function ondown(event){
   
 }
 function onup(event){
   
 }
-canvas.addEventListner("mousedown",ondown,false)
-canvas.addEventListner("mouseup",onup,false)
+cvs.addEventListner("mousedown",ondown,false);
+cvs.addEventListner("mouseup",onup,false);
+function draw(){
+    var cvs = document.getElementById("minesweeper");
+    var context　= cvs.getContext("2d");
+    context.drawImage(base,40,40);
+    context.drawImage(ground,40,80);
+}
+function reset(){
+    draw();
+}
+reset();
